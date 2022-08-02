6ba00a7a3535733a62c7d11f93ad36ac0514ee98
Jason Swinehart <jaswine@comcast.net>
2022-08-01 20:47:09 -0400

    add clever solution

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -22,11 +22,11 @@ var twoSum = function(nums, target) {
  
 Below is the "slick" answer because we check the map before the value is loaded,
 which means the first number in the pair won't be found, but that is OK because
-we will find it once we get to the second number. The point of this is so there is no self-matching (ex. index0 and index0)
-which would be invalid, so there is no need to do a check like on line 14. And we can do it in one loop.
+we will find it once we get to the second number. The point of this is so there is no self-matching (ex. index0 + index0 = target)
+which would be invalid (must be two different indices), so there is no need to do a check like on line 14. And we can do it in one loop.
  */
 
-var twoSum2 = function(nums, target) {          
+var twoSum = function(nums, target) {          
     const map = new Map();                      
     for(let i = 0; i < nums.length; i++){      
         if (map.has(target - nums[i])) return [i, map.get(target - nums[i])]    
