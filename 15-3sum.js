d24f2776c82a62b49343c81b7ffdbf09a9af0cd6
Jason Swinehart <jaswine@comcast.net>
2022-08-10 22:48:38 -0400

    comments

--- 15-3sum.js
+++ 15-3sum.js
@@ -16,16 +16,6 @@ let nums3 = [1,-1,-1, -1, 0, 0]
     let j, k; // num[j] left pointer, num[i] is right pointer
 
     for (let i = 0; i < nums.length-2; i++){
-
-        // if ((nums[i] === 0) && (nums[i+1] == 0)){
-        //     if (nums[i+2] === 0){
-        //         if (nums[i+3] === 0) continue
-        //         else{
-        //             arr.push([0, 0, 0])
-        //             continue
-        //         }
-        //     }
-        // }
         if (nums[i] == nums[i - 1]) continue
         j = i + 1;
         k = nums.length - 1;
@@ -35,7 +25,6 @@ let nums3 = [1,-1,-1, -1, 0, 0]
             if (nums[i] + nums[j] + nums[k] > 0) k--
             else if (nums[i] + nums[j] + nums[k] < 0) j++
             else {
-                console.log('Bingo')
                 arr.push([nums[i], nums[j], nums[k]])
                 j++
                 while ((j < k)&&(nums[j] === nums[j-1])) j++
---
+++ 167-two-sum-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/*
+
+
+Left pointer i
+Right pointer j
+
+while i < j:
+
+    check the first and last index/ numbers at i and j
+    if the sum is less than than target: i++
+    elif the sum is greater than the target: j--
+    else: the sum == target, return indices
+
+*/
+
+ var twoSum = function(numbers, target) {
+    
+    let i = 0; let j =  (numbers.length - 1);
+
+    while (i < j){
+        if (numbers[i] + numbers[j] < target) i++
+        else if (numbers[i] + numbers[j] > target) j--
+        else return [i+1, j+1]
+
+    }
+
+};
+
+console.log(twoSum([2,7,9,10,14,15], 19));
