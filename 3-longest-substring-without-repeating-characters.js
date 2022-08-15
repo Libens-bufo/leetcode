e5320d0111b4157851055421b22e3f5a342882a6
Jason Swinehart <jaswine@comcast.net>
2022-08-14 21:15:17 -0400

    comments

--- 3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters.js
@@ -4,11 +4,11 @@ var lengthOfLongestSubstring = function(s) {
     let set = new Set()
     
     for (let r = 0; r < s.length; r++){
-        while (set.has(s[r])){
-            set.delete(s[l])
-            l++
+        while (set.has(s[r])){                  //we have reached a repeating character
+            set.delete(s[l])                    // delete the far left
+            l++                                 //continue until its a substring without repeats
         }
-        set.add(s[r])
+        set.add(s[r])                           
         max = Math.max(max, set.size)
     }
     return max
