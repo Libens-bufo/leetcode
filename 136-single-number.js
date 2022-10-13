2ba7280a17bc7964412efe931a50ff7210880e24
Jason Swinehart <jaswine@comcast.net>
2022-10-13 19:09:05 -0400

    add solution to this bit manipulation problem

---
+++ 136-single-number.js
@@ -0,0 +1,26 @@
+// nums = [2, 2, 1] => 1
+// nums = [2, 3, 3, 4, 4] => 2
+
+//function takes a parameter 'nums', an array. Length?
+//function finds the single number and returns the number
+//Solution must be O(n) and use constant space.
+
+function singleNumber (nums){
+    let result = 0;
+    for (let i = 0; i < nums.length; i++){
+        result = result ^ nums[i];
+    }
+    return result;
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
