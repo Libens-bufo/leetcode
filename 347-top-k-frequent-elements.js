48ac235bfa8bf4ffc6d09196e125cfe5776d6490
Jason Swinehart <jaswine@comcast.net>
2022-10-28 21:38:28 -0400

    pseudocode

---
+++ 347-top-k-frequent-elements.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+ var topKFrequent = function(nums, k) {
+    //Initialize a hash map/object.
+    //Loop over array nums, and count each number.
+    //If number is not in map, add to map with count of 1
+    //If number is in map, increment count
+    //----
+    //Initialize an array and fill it.
+    //Iterate through the map: The key gets pushed to the array index === value of key/val
+    //Iterate backwards through array to get the k most frequent in order
+  
+ }
