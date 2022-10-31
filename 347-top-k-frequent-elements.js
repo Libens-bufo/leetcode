7f2ee58eb683d1af37a06105d7f3b03f3aeadf4f
Jason Swinehart <jaswine@comcast.net>
2022-10-30 21:39:56 -0400

    solution

--- 347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements.js
@@ -13,5 +13,27 @@
     //Iterate through the map: The key gets pushed to the array index === value of key/val
     //Iterate backwards through array to get the k most frequent in order
   
- }
- 
+    const map = {};
+    for (let number of nums){
+        if (!map[number]) map[number] = 1;
+        else{
+            map[number] = map[number] + 1;
+        }
+    }
+  
+    const bucket = Array(nums.length + 1).fill().map(el => []); 
+    console.log(map)
+    for (let [key, value] of Object.entries(map)){
+        bucket[value].push(+key);
+    }
+  
+    const result = []
+    for(let i = bucket.length - 1; i >= 0; i--){
+      if (bucket[i].length){
+        bucket[i].forEach((el) => {
+          if (result.length < k) result.push(el)
+        })
+        if (result.length == k) return result;
+      }
+    }
+  };
